d1cdeaae4a8d97a59cc4e63c02d5695f8acaf929
Max Osipov <osipovmax282@gmail.com>
2022-08-05 03:09:46 +0300

    Server setup fix

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/taskamager-frontend'));
 
-app.get('/*', function(req,res) {   
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function(req ,res) {   
+    res.sendFile(path.join(__dirname+'/dist/taskmanager-frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
