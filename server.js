45ca9d1e80248cbd9fdf731b57def2dccaae72ec
Max Osipov <osipovmax282@gmail.com>
2022-08-05 03:34:22 +0300

    Server setup fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist'), { index: false }));
+app.use(express.static(path.join(__dirname, 'dist', 'taskmanager-frontend')));
 
 app.get('/*', function(req, res) {   
     res.sendFile(path.join(__dirname, 'dist', 'taskmanager-frontend', 'index.html'));
